30ec10b50056ec59cc7f7532a9f378c148a2a04a
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-13 14:01:03 -0500

    Add files via upload

---
+++ abs_dif.py
@@ -0,0 +1,11 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def abs_dif(x,y):
+	"""(number,number)->number
+        Return the absolute value of the difference between
+        x and y
+
+        >>>abs_dif(10,4)
+        6
+        """
+	return abs(x-y)
